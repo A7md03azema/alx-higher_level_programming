e60d6c13317f7f7ba92e70db940f54cd062ddd3d
A7md03azema <ahmedabdelaziem696@gmail.com>
2023-08-16 05:23:26 +0300

    args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    count = len(sys.argv) - 1
+    if count == 0:
+        sufix = "arguments."
+    elif count == 1:
+        sufix = "argument:"
+    else:
+        sufix = "arguments:"
+    print("{} {}".format(count, sufix))
+    for index, arg in enumerate(sys.argv, start=0):
+        if index != 0:
+            print("{}: {}".format(index, arg))
